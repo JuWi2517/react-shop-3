cc5d8a11c27e238762a1db22c8f6852a011a97db
JuWi2517 <jan.richter@student.spsezatec.cz>
2023-12-17 21:30:36 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Card from "./Card";
+import NavBar from "./NavBar";
+import ShopList from "./ShopList";
+import { Routes, Route, Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <NavBar></NavBar>
+        <h1>Přehled</h1>
+        <div className="listButtons">
+            <button id="all" type="button" className="btn btn-primary">Zobrazit všechny</button>
+            <button type="button" className="btn btn-success">Vytvořit nový seznam</button>
+        </div>
+        <div className="shopList">
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+        </div>
+
+        <li><Link to="/ShopList">Shop</Link></li>
+
+        <Routes>
+            <Route path="/ShopList" element={<ShopList />} />
+        </Routes>
     </div>
+
   );
 }
 
